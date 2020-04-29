859f19780ccac7baf52437215f842f26b6ccaeff
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-04-29 12:15:12 -0400

    precompile assets attempt 3

--- config/application.rb
+++ config/application.rb
@@ -11,11 +11,11 @@ Bundler.require(*Rails.groups)
 module Gleam
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    # config.load_defaults 6.0
 
     # added to April 29, 2020 to attempt successful heroku push
     # try to comment out config.load_defaults and then only this next time
-    # config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
